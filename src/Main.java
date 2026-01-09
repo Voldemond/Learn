2f5023e63a3d56546961cf5e643e5293e3e60052
Voldy13 <136642925+Voldemond@users.noreply.github.com>
2026-01-09 15:09:42 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+            String greets = "Hello ";
+            int greetlength = greets.length();
+
+            int num = 4;
+
+            int likes = 4035 ;
+            int dislikes = 40 ;
+
+            double total = MathUtils.ratio(likes, dislikes );
+
+
+        System.out.println(total);
+
+    }
+
+}
